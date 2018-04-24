1713115d497d6869d027b26fa58c13e4e688d2dd
sun0x00 <sun0x00@gmai.com>
2018-04-24 14:43:46 +0800

    首次导入

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'sun0x00'
+SITENAME = u'RedTorch'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'Chinese (Simplified)'
+DEFAULT_CATEGORY = 'MAIN'
+DEFAULT_DATE_FORMAT = '%Y %m %d'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('VNPY', 'http://www.vnpy.org'),
+         ('Python.org', 'http://python.org/'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
